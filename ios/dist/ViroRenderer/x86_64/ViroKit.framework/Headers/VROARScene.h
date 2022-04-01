6f2269fd1378d4f990c260d13d75ee140e780598
Adam Butler <ab@adam-butler.com>
2022-04-01 15:41:18 +0200

    fix: try setting more defaults to PlanesVertical

--- ios/dist/ViroRenderer/x86_64/ViroKit.framework/Headers/VROARScene.h
+++ ios/dist/ViroRenderer/x86_64/ViroKit.framework/Headers/VROARScene.h
@@ -56,7 +56,7 @@ public:
             _ambientLightColor = { 1.0, 1.0, 1.0 };
             _pointCloudNode = std::make_shared<VRONode>();
             _pointCloudEmitter = nullptr;
-            _detectionTypes = { VROAnchorDetection::PlanesHorizontal }; //default is horizontal
+            _detectionTypes = { VROAnchorDetection::PlanesVertical }; // set default to vertical
         };
     virtual ~VROARScene();
     
